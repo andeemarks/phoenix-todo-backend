b87444834a0b0df6860fad15628295aea0892e0b
Andy Marks <amarks@thoughtworks.com>
2015-04-03 22:27:48 +1100

    Adding OPTIONS, POST and DELETE support to root for testing

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,9 @@ defmodule TodoBackend.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    post "/", PageController, :index
+    options "/", PageController, :index
+    delete "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
